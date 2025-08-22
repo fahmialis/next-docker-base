847e1b62e8197a4814e110acdce2bb5304237df1
fahmialis <fsinergia@yahoo.com>
2025-08-22 15:07:24 +0700

    test husky

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+// .eslintrc.js
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser', // 👈 use TS parser
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals', // 👈 Next.js ESLint rules
+  ],
+  rules: {
+    semi: ['warn', 'always'],
+  },
+};
--- app/(features)/login/page.tsx
+++ app/(features)/login/page.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
 export default function LoginPage() {
+  console.log('test ajaa');
   return (
-    <div className='flex items-center gap-5 w-full'>
+    <div className="flex items-center gap-5 w-full">
       ini login page
+      <span>asdfasdf</span>
     </div>
-  )
+  );
 }
